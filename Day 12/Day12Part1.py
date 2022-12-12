02d0f65be2a5d82f974b861353f1c105ced3e361
lantisescudo <52919745+lantisescudo@users.noreply.github.com>
2022-12-12 09:02:43 -0500

    Added Day 12

---
+++ Day 12/Day12Part1.py
@@ -0,0 +1,67 @@
+inputfile = "C:\\Users\\Lantis\\Documents\\GitHub\\Advent-of-Code-2022\\Day 12\\fullinput.txt"
+ifilehandle = open(inputfile,"r")
+
+grid = []
+
+rownum = -1
+
+startpos = []
+finishpos = []
+
+for line in ifilehandle:
+    rownum += 1
+    row = []
+    colnum = -1
+    for char in line.strip():
+        colnum += 1
+        if char == 'S':
+            height = 0
+            startpos = (rownum,colnum)
+        elif char == 'E':
+            height = 25
+            finishpos = (rownum,colnum)
+        else:
+            height = ord(char)-97
+
+        row.append(height)
+    
+    grid.append(row)
+
+stepcount = 0
+poslist = [startpos]
+walking = True
+visited = []
+
+while walking:
+    destinations = []
+    stepcount += 1
+    for item in poslist:
+        visited.append(item)
+        #Up
+        if (item[0] != 0) and (grid[item[0]-1][item[1]] <= (grid[item[0]][item[1]]+1)):
+            target = (item[0]-1,item[1])
+            if (target not in destinations) and (target not in visited):
+                destinations.append(target)
+        #Down
+        if (item[0] != len(grid)-1) and (grid[item[0]+1][item[1]] <= (grid[item[0]][item[1]]+1)):
+            target = (item[0]+1,item[1])
+            if (target not in destinations) and (target not in visited):
+                destinations.append(target)
+        #Left
+        if (item[1] != 0) and (grid[item[0]][item[1]-1] <= (grid[item[0]][item[1]]+1)):
+            target = (item[0],item[1]-1)
+            if (target not in destinations) and (target not in visited):
+                destinations.append(target)
+        #Right
+        if (item[1] != len(grid[0])-1) and (grid[item[0]][item[1]+1] <= (grid[item[0]][item[1]]+1)):
+            target = (item[0],item[1]+1)
+            if (target not in destinations) and (target not in visited):
+                destinations.append(target)
+        
+        if finishpos in destinations:
+            walking = False
+            break
+    
+    poslist = destinations
+
+print(stepcount)
